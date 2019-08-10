182d0ba0a56c9582792e534333abc10a8e0be0ac
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-10 21:43:53 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = blogs => 1
+
+
+const totalLikes = blogs => blogs
+  .map(blog => blog.likes)
+  .reduce((total, amount) => total + amount, 0)
 
 module.exports = {
   dummy,
+  totalLikes,
 }
