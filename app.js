7d3ad0185cc185e295ae9c38fe290f32cdfd83a6
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-10 13:23:58 +0300

    Paty 4-2

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 
-const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
-
 const blogsRouter = require('./controllers/blogs')
+
+const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+
+morgan
+  .token('body', req => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 console.log('connecting to', config.MONGODB_URI)
 
